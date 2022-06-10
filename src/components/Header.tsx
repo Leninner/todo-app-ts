3be43543c3e990fb23197addfa59fa2e3f8bd9ca
leninner <mazabandalenin180@gmail.com>
2022-06-09 21:19:01 -0500

    Añadida lógica para mover el botón al hacer hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,49 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { ThemeContext } from '../context/DarkTheme'
 
 export const Header = () => {
-  const { isDark, toggleTheme }: any = useContext(ThemeContext)
   const buttonRef = useRef<HTMLButtonElement>(null)
-  const [currentTop, setCurrentTop] = useState(20)
-  const [currentLeft, setCurrentLeft] = useState(150)
+
+  const [currentBoxPosition, setCurrentBoxPosition] = useState({
+    top: buttonRef.current?.offsetTop,
+    left: buttonRef.current?.offsetLeft,
+    widthBox: buttonRef.current?.offsetWidth,
+    heightBox: buttonRef.current?.offsetHeight,
+  })
+
+  const { isDark, toggleTheme }: any = useContext(ThemeContext)
+
+  const widthScreen = window.innerWidth
+  const heightScreen = window.innerHeight
+
+  const handleChangePosition = (e: any) => {
+    const button = buttonRef.current
+
+    if (button) {
+      const top = Math.random() * (heightScreen - 200)
+      const left = Math.random() * (widthScreen - 250)
+
+      setCurrentBoxPosition({
+        ...currentBoxPosition,
+        top: top,
+        left: left,
+      })
+    }
+  }
 
   return (
-    <div className="w-full h-screen dark:bg-slate-800 bg-gray-100 relative">
+    <div className="w-full h-screen dark:bg-slate-800 bg-gray-100 relative flex items-center justify-center">
       <button
-        className={`outline-none absolute top-[${currentTop}px] left-[${currentLeft}px]`}
+        className={`outline-none absolute`}
+        style={{ top: currentBoxPosition.top, left: currentBoxPosition.left }}
         ref={buttonRef}
+        onMouseEnter={handleChangePosition}
       >
         <div
           className="w-auto h-auto  text-white dark:text-slate-800 font-bold py-10 px-20 rounded-lg bg-slate-800 dark:bg-white shadow-lg shadow-gray-500"
           onClick={toggleTheme}
         >
-          {isDark ? (
-            <p>Sin derecho? :,c</p>
-          ) : (
-            <p>
-              Con Derecho? <br /> UwU <br /> 7u7
-            </p>
-          )}
+          {isDark ? <p>Sin derecho? :,c</p> : <p>No</p>}
         </div>
       </button>
     </div>
