4d4ec924a2a50c0c4c4034c593e5f058f1c4c786
leninner <mazabandalenin180@gmail.com>
2022-06-29 13:46:51 -0500

    Added styles and I improve the code quality

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,25 +1,63 @@
-export const InputComponent = ({ options, disabled, text, completed, onChange, onClick }: any) => {
+import { useState } from 'react'
+
+export const InputComponent = ({
+  optionsStyles,
+  disabled,
+  text,
+  completed,
+  onChange,
+  onClick,
+}: any) => {
+  const [isHover, setIsHover] = useState(false)
+  const optionsInput = {
+    disabled,
+    text,
+    completed,
+    onChange,
+  }
+
+  const handleMouseEnter = () => setIsHover(true)
+  const handleMouseLeave = () => setIsHover(false)
+
   return (
-    <div
-      className={`w-full py-5 px-2 rounded-md dark:bg-dark_veryDarkDesaturatedBlue bg-light_veryLightGray flex items-center justify-between ${options?.className}`}
+    <section
+      className={`w-full py-5 px-2 rounded-md dark:bg-dark_veryDarkDesaturatedBlue bg-light_veryLightGray flex items-center justify-between relative ${optionsStyles?.className}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {disabled ? (
-        onClick()
+        <>
+          {isHover && (
+            <span
+              className={`absolute text-4xl dark:text-dark_veryDarkGrayishBlue z-20 right-5 text-light_darkGrayishBlue cursor-pointer`}
+            >
+              x
+            </span>
+          )}
+
+          {onClick()}
+        </>
       ) : (
         <div
           className={`w-6 h-6 rounded-full mr-5 ml-3 border border-dark_lightGrayishBlue dark:border-light_veryDarkGrayishBlue `}
         />
       )}
 
-      <input
-        type="text"
-        placeholder="Add a todo"
-        className={`outline-none flex-1 px-2 bg-transparent dark:text-dark_lightGrayishBlue placeholder:dark:text-dark_darkGrayishBlue caret-primary_brigthBlue
-          ${completed && 'line-through text-light_darkGrayishBlue dark:text-dark_darkGrayishBlue'}`}
-        value={text}
-        disabled={disabled}
-        onChange={onChange}
-      />
-    </div>
+      <Input {...optionsInput} />
+    </section>
+  )
+}
+
+const Input = ({ disabled, text, completed, onChange }: any) => {
+  return (
+    <input
+      type="text"
+      placeholder="Add a todo"
+      className={`outline-none flex-1 px-2 bg-transparent dark:text-dark_lightGrayishBlue placeholder:dark:text-dark_darkGrayishBlue caret-primary_brigthBlue
+      ${completed && 'line-through text-light_darkGrayishBlue dark:text-dark_darkGrayishBlue'}`}
+      value={text}
+      disabled={disabled}
+      onChange={onChange}
+    />
   )
 }
--- src/containers/TodoList.tsx
+++ src/containers/TodoList.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { InputComponent } from '../components/TodoItems'
 import { TodosContext } from '../context/TodosContext'
 import { TodoInterface } from '../interfaces'
@@ -6,30 +6,34 @@ import { TodoInterface } from '../interfaces'
 export const TodoList = () => {
   const { todos, handleCompleteTodo }: any = useContext(TodosContext)
 
-  const options = {
+  const optionsStyles = {
     className:
       'rounded-none border-b dark:border-light_veryDarkGrayishBlue border-light_lightGrayishBlue',
   }
 
   return (
-    <div className="shadow-2xl shadow-light_darkGrayishBlue dark:shadow-dark_veryDarkBlue">
+    <section className="shadow-2xl shadow-light_darkGrayishBlue dark:shadow-dark_veryDarkBlue">
       {todos.map((todo: TodoInterface) => (
         <InputComponent
-          options={options}
+          optionsStyles={optionsStyles}
           disabled
           {...todo}
           key={todo.id}
-          onClick={() => (
-            <div
-              className={`w-6 h-6 rounded-full mr-5 ml-3 border border-dark_lightGrayishBlue dark:border-light_veryDarkGrayishBlue ${
-                todo.completed &&
-                'bg-gradient-to-r from-primary_checkBackgroundStart to-primary_checkBackgroundEnd'
-              }`}
-              onClick={() => handleCompleteTodo(todo.id)}
-            />
-          )}
+          onClick={() => <BurbujaComponent handleCompleteTodo={handleCompleteTodo} {...todo} />}
         />
       ))}
-    </div>
+    </section>
+  )
+}
+
+const BurbujaComponent = ({ handleCompleteTodo, completed, id }: any) => {
+  return (
+    <div
+      className={`w-6 h-6 rounded-full mr-5 ml-3 border border-dark_lightGrayishBlue dark:border-light_veryDarkGrayishBlue ${
+        completed &&
+        'bg-gradient-to-r from-primary_checkBackgroundStart to-primary_checkBackgroundEnd'
+      }`}
+      onClick={() => handleCompleteTodo(id)}
+    />
   )
 }
